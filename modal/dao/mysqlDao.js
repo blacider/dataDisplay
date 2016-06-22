b6679f881483989ff918bb0464693b570c595770
niurenpeng <niurenpeng@niurenpeng.com>
2016-06-23 00:15:58 +0800

    mysql 抽象类

---
+++ modal/dao/mysqlDao.js
@@ -0,0 +1,20 @@
+// dao/caseDao.js
+// 实现与MySQL交互
+var mysql = require('mysql');
+var $conf = require('../conf/db');
+var $util = require('../util/util');
+var pool  = mysql.createPool($util.extend({}, $conf.mysql));
+
+
+var queryAll: 'select * from ';
+
+module.exports = {
+    queryAll: function (tableName, callback) {
+        pool.getConnection(function(err, connection) {
+            connection.query(queryAll+tableName, function(err, result) {
+                callback(err, result);
+                connection.release();
+            });
+        });
+    }
+};
