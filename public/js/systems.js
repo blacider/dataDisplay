dbca655150a2d12d9897a43e9433a856cdf7da30
niurenpeng <niurenpeng@niurenpeng.com>
2016-09-20 19:12:27 +0800

    商事信息增加点击行显示详细信息

--- public/js/systems.js
+++ public/js/systems.js
@@ -15,7 +15,38 @@
                         getData(pag.getCurrent());
                     }
                 });
+            };
+            if (n == "gs") {
+                $("body").delegate('.tbody tr', 'click', function(event) {
+                    showLoading();
+                    $.ajax({
+                      url: '/xx',
+                      type: 'GET',
+                      dataType: 'html',
+                      data: {
+                        zch: $(event.target).closest('tr').children().eq(1).text(),
+                        n:'jb',
+                        name:$(event.target).closest('tr').children().eq(2).text()
+                      }
+                    })
+                    .done(function(data) {
+                      $(".close").next().empty().append(data).closest('.modal').show();
+                      $(".modal").height($(".close").next().height());
+                      hideLoading();
+                    });
+                });
             }
+            $(".modal").delegate('li', 'click', function(event) {
+              $(".modal table").removeClass('active');
+              $(".modal li").removeClass('active');
+              $(event.target).addClass('active');
+              $(".modal #table_"+$(event.target).data('id')).addClass('active');
+              $(".modal").height($(".close").next().height());
+            });
+            $('.close').click(function(event) {
+              /* Act on the event */
+              $('.modal').hide();
+            });
             $("#search button").click(function(event) {
                 if (searchText != $(this).closest('#search').find('input').val()) {
                     showLoading();
